37488108bb21101937a0e71d8602ad9a31b248cb
Gmaxhacker1 <151476499+Gmaxhacker1@users.noreply.github.com>
2024-11-08 23:07:10 +0300

    Update and rename bmwt.js to gmaxt.js

--- scs/gmaxt.js
+++ scs/gmaxt.js
@@ -13,7 +13,7 @@ function list(text, fancy) {
     let msg = '\n*Availabe Fancy styles for BMW-MD:*\n\n';
     for (let style in styles) {
         if (style == 33) {
-            msg += (parseInt(style) + 1) + '. ' + 'bmw-md' + '\n';
+            msg += (parseInt(style) + 1) + '. ' + 'gmax xmd' + '\n';
         } else {
             msg += (parseInt(style) + 1) + '. ' + fancy.apply(fancy[parseInt(style)], text) + '\n';
         }
