c06650e55c06427bf2ea124c9f70698f6af7e170
Gmaxhacker1 <151476499+Gmaxhacker1@users.noreply.github.com>
2024-11-21 12:58:20 +0300

    Update mesfonctions.js

--- watson/mesfonctions.js
+++ watson/mesfonctions.js
@@ -9,7 +9,7 @@ const baileys_1 = require("@whiskeysockets/baileys");
 const fs = require('fs-extra');
 const util = require('util');
 let { listall } = require('./stylish-font');
-/*_________by Djalega++
+/*_________by Mselachui 
 
 fonction zJson:
 récupère un objet json
@@ -32,7 +32,7 @@ module.exports.genererNomFichier = async (extension) => {
 /** ************ */
 module.exports.stick = async (buffer, author) => {
     var sticker = new Sticker(buffer, {
-        pack: 'BMW-MD',
+        pack: 'GMAX-XMD-V2',
         author: author,
         type: StickerTypes.FULL,
         categories: ['🤩', '🎉'],
